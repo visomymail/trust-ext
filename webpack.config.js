9ac09e6b8b5a7d56808ec1f3a016dc2ab0a49ba6
artem <=>
2021-02-05 15:25:58 +0700

    add: copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
         background: path.resolve(__dirname, 'src/extension/background/index.ts'),
+        popup: path.resolve(__dirname, 'src/extension/popup/index.ts'),
         content: path.resolve(__dirname, 'src/extension/content/index.ts')
     },
     output: {
@@ -20,5 +22,14 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'assets/manifest.json'},
+                {from: 'assets/popup.html'},
+                {from: 'assets/images/logo.png'}
+            ]
+        })
+    ]
 };
